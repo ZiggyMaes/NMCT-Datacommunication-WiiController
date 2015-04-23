6eb16f9e819bb5cee1da6db50ca3f4a24b57292c
Ziggy Maes <ziggymaes@telenet.be>
2015-04-23 14:39:33 +0200

    +Added BT connectivity to controller +4 button DPAD status view on GUI

---
+++ Wii-Remote-Project/Testtool/frmMain.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Testtool
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDiretionUp = new System.Windows.Forms.Button();
+            this.btnDirectionDown = new System.Windows.Forms.Button();
+            this.btnDirectionRight = new System.Windows.Forms.Button();
+            this.btnDirectionLeft = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnDiretionUp
+            // 
+            this.btnDiretionUp.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnDiretionUp.Location = new System.Drawing.Point(242, 53);
+            this.btnDiretionUp.Name = "btnDiretionUp";
+            this.btnDiretionUp.Size = new System.Drawing.Size(40, 90);
+            this.btnDiretionUp.TabIndex = 1;
+            this.btnDiretionUp.UseVisualStyleBackColor = false;
+            // 
+            // btnDirectionDown
+            // 
+            this.btnDirectionDown.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnDirectionDown.Location = new System.Drawing.Point(242, 177);
+            this.btnDirectionDown.Name = "btnDirectionDown";
+            this.btnDirectionDown.Size = new System.Drawing.Size(40, 90);
+            this.btnDirectionDown.TabIndex = 2;
+            this.btnDirectionDown.UseVisualStyleBackColor = false;
+            // 
+            // btnDirectionRight
+            // 
+            this.btnDirectionRight.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnDirectionRight.Location = new System.Drawing.Point(279, 140);
+            this.btnDirectionRight.Name = "btnDirectionRight";
+            this.btnDirectionRight.Size = new System.Drawing.Size(90, 40);
+            this.btnDirectionRight.TabIndex = 3;
+            this.btnDirectionRight.UseVisualStyleBackColor = false;
+            // 
+            // btnDirectionLeft
+            // 
+            this.btnDirectionLeft.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnDirectionLeft.Location = new System.Drawing.Point(154, 140);
+            this.btnDirectionLeft.Name = "btnDirectionLeft";
+            this.btnDirectionLeft.Size = new System.Drawing.Size(90, 40);
+            this.btnDirectionLeft.TabIndex = 4;
+            this.btnDirectionLeft.UseVisualStyleBackColor = false;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(651, 487);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 5;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1208, 1391);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnDirectionLeft);
+            this.Controls.Add(this.btnDirectionRight);
+            this.Controls.Add(this.btnDirectionDown);
+            this.Controls.Add(this.btnDiretionUp);
+            this.Name = "frmMain";
+            this.Text = "Wii Remote Testtool";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDiretionUp;
+        private System.Windows.Forms.Button btnDirectionDown;
+        private System.Windows.Forms.Button btnDirectionRight;
+        private System.Windows.Forms.Button btnDirectionLeft;
+        private System.Windows.Forms.Button btnRefresh;
+    }
+}
+
---
+++ Wii-Remote-Project/Testtool/frmMain.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WII.HID.Lib;
+
+namespace Testtool
+{
+    public partial class frmMain : Form
+    {
+        HIDDevice _device;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            init_controller();
+            HIDReport report = _device.CreateReport();
+            report.ReportID = 0x12;
+            report.Data[0] = (byte)0x04;
+            report.Data[1] = (byte)0x30;
+            _device.WriteReport(report);
+        }
+
+        private void init_controller()
+        {
+            _device = HIDDevice.GetHIDDevice(0x57E, 0x306);
+        }
+
+        private void OnReadReport(HIDReport report)
+        {
+            if (this.InvokeRequired) { this.Invoke(new ReadReportCallback(OnReadReport), report); }
+            else
+            {
+                int dpad_value = report.Data[0]; //Left: 1 - Right: 2 - Down: 4 - Up: 8
+                switch (dpad_value)
+                {
+                    case 1: //Left
+                        btnDirectionLeft.BackColor = Color.Red;
+                        break;
+                    case 2: // Right
+                        btnDirectionRight.BackColor = Color.Red;
+                        break;
+                    case 4: //Down
+                        btnDirectionDown.BackColor = Color.Red;
+                        break;
+                    case 5: //left-down
+                        btnDirectionLeft.BackColor = Color.Red;
+                        btnDirectionDown.BackColor = Color.Red;
+                        break;
+                    case 6: //right-down
+                        btnDirectionRight.BackColor = Color.Red;
+                        btnDirectionDown.BackColor = Color.Red;
+                        break;
+                    case 8: //Up
+                        btnDiretionUp.BackColor = Color.Red;
+                        break;
+                    case 9: //left-up
+                        btnDirectionLeft.BackColor = Color.Red;
+                        btnDiretionUp.BackColor = Color.Red;
+                        break;
+                    case 10: //right-up
+                        btnDirectionRight.BackColor = Color.Red;
+                        btnDiretionUp.BackColor = Color.Red;
+                        break;
+                    default:
+                        btnDirectionLeft.BackColor = Color.White;
+                        btnDirectionRight.BackColor = Color.White;
+                        btnDiretionUp.BackColor = Color.White;
+                        btnDirectionDown.BackColor = Color.White;
+                        break;
+
+
+                    
+                }
+
+                _device.ReadReport(OnReadReport);
+            }
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            _device.ReadReport(OnReadReport);
+        }
+    }
+
+}
